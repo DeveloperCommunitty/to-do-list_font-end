fd759e26867c47d7617f98d2f2073c6c45464baf
guilhermeprog3 <guilhermeriosprog@gmail.com>
2025-01-13 10:33:00 -0300

    feat:Tirando as margens do Google

--- src/routes/routerManage.jsx
+++ src/routes/routerManage.jsx
@@ -1,24 +1,28 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import { useMemo } from 'react'
-import { RoutesPath } from './Routepath'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useMemo } from 'react';
+import { RoutesPath } from './Routepath';
+import { CssBaseline, GlobalStyles } from '@mui/material';
 
+export const RouterManager = () => {
+  const route = useMemo(() => 
+    Object.keys(RoutesPath).map((path) => {
+      const RouteComponent = RoutesPath[path];
+      return (
+        <Route key={path} path={path} element={<RouteComponent />} />
+      );
+    }),
+    []
+  );
 
-export const RouterManager = () =>{
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    const route = useMemo (
-        () => Object.keys(RoutesPath).map((path) => {
-            const RouteComponent = RoutesPath[path];
-            return(
-                <Route key={path} path={path} element={<RouteComponent />} />
-            )
-        })
-        
-    )
-    return(
-        <BrowserRouter>
-         <Routes>
-            {route}
-         </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <>
+      <CssBaseline />
+      <GlobalStyles tyles={{ body: { margin: 0, padding: 0 }, htsml: { margin: 0, padding: 0 } }} />
+      <BrowserRouter>
+        <Routes>
+          {route}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
